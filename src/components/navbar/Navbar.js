d98b64930710cc234144eececdf3d1bffbf03792
Ehsan <dev.ehsanhosseini@gmail.com>
2023-12-08 15:35:27 +0330

    menu func added

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Desktop from './Desktop'
 import Hamburger from './Hamburger'
 import { Link } from 'react-router-dom';
 
 
 export default function Navbar() {
+
+  let [isHamburgurOpen , setIsHamburgurOpen] = useState(false)
+  
+  function menuFunc (){
+    if(isHamburgurOpen){setIsHamburgurOpen(!isHamburgurOpen)}else{setIsHamburgurOpen(!isHamburgurOpen)}   
+        
+    console.log(isHamburgurOpen)
+  }
   return (
     <nav className="desktop-nav flex justify-around items-center h-[17vh]">
-      
+        
           <Link className='logo text-3xl lg:font-bold cursor-pointer' to="/" >EHSAN</Link>
           {/* <div className="logo text-3xl lg:font-bold">EHSAN</div> */}
 
@@ -18,9 +26,9 @@ export default function Navbar() {
                 <Desktop address={"/contact"} content={"Contact"}/>
         </div>
         <div className="hamburgur-nav flex lg:hidden relative">
-            <span id="menu" className="flex material-symbols-outlined w-8 cursor-pointer">menu</span>
-            <span id="close" className="hidden material-symbols-outlined w-8 cursor-pointer">close</span>
-                <div className="hamburgur-menu z-50 absolute hidden top-5 right-4 flex-col justify-center items-center bg-white border border-solid border-gray-400">
+            <span id="menu" className={`${isHamburgurOpen ? "hidden" :"flex"} material-symbols-outlined w-8 cursor-pointer`} onClick={menuFunc}>menu</span>
+            <span id="close" className={`${isHamburgurOpen ? "flex" :"hidden"} material-symbols-outlined w-8 cursor-pointer`} onClick={menuFunc}>close</span>
+                <div className={`hamburgur-menu z-50 absolute ${isHamburgurOpen ? "flex" :"hidden"} top-5 right-4 flex-col justify-center items-center bg-white border border-solid border-gray-400`}>
                     <Hamburger address={"/about"} content={"About"}/>
                     <Hamburger address={"/experience"} content={"Experience"}/>
                     <Hamburger address={"/projects"} content={"Projects"}/>
