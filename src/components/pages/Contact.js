5ee1caab02976869940713982fe373bb7706eccf
Ehsan <dev.ehsanhosseini@gmail.com>
2023-12-09 17:22:32 +0330

    contact item edited

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -10,14 +10,13 @@ function Contact() {
     <div className="contact-boxs flex items-start justify-center">
         {/* <!-- res  --> */}
         <div className="project-box lg:w-[450px] w-[340px] flex flex-col items-center justify-center p-5 lg:p-5 rounded-[32px] border border-solid border-gray-400 ">
-            <div className="btn flex flex-wrap gap-10 justify-center items-center">
+            <div className="btn flex flex-wrap gap-x-8 gap-y-4 justify-center items-center">
                 <div className="flex justify-center items-center  gap-x-1.5">
                     <img src="./email.png" alt="" className="w-8"/>
                     <p className="text-base lg:text-lg font-medium hover:text-gray-500  cursor-pointer transition-all box-border">Dev.EhsanHosseini@Gmail.com</p>
                 </div>
                 <div className="flex justify-center items-center gap-x-1.5">
                 <i className="fa-brands fa-github text-3xl"></i>
-                
                     <p className="text-base lg:text-lg font-medium hover:text-gray-500  cursor-pointer transition-all box-content"><Link to="https://github.com/seyedehsandev">Github</Link></p>
                 </div>
             </div>
